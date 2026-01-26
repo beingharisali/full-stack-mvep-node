c23d80c54b819a86515980e75dec6f88eb23bfca
Iqra yasmeen <yasirali4363@gmail.com>
2026-01-26 19:26:38 +0500

    validation add

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose")
 const categorySchema = new mongoose.Schema({
   name:{
     type: Strirng,
+    required: true,
+    unique: true,
+    trim: true,
+    maxlength: 32
   },
-
   description: {
-      type: String
-  
+      type: String,
+      maxlength: 2000
   }
 }, { timestamps: true});
 module.exports = mongoose.model("Category", categorySchema);
