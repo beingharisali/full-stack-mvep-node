4d83d4d1159d03e7a3971b1a559639abe7a82e2e
Iqra yasmeen <yasirali4363@gmail.com>
2026-01-28 22:16:03 +0500

    create subCategory

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,10 @@
+const mongoose = require("mongoose")
 const Category = require("../models/Category")
+const Subcategory = require("../models/Subcategory")
+
 const createCategory = async (req, res) => {
     try {
-        const { name,description} = req.body;
+        const { name, description } = req.body;
         const category = await Category.create({
             name,
             description
@@ -11,4 +14,30 @@ const createCategory = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
-module.exports = { createCategory }
+const createSubCategory = async (req, res) => {
+    try {
+    const { name, category } = req.body;
+
+    
+    if (!mongoose.Types.ObjectId.isValid(category)) {
+      return res.status(400).json({
+        error: "Invalid category ID"
+      });
+    }
+
+   
+    const sub = await Subcategory.create({
+      name,
+      category
+    });
+
+    res.status(201).json({
+      success: true,
+      sub
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    }
+}
+module.exports = { createCategory, createSubCategory }
--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory } = require("../controllers/category")
+const { createCategory, createSubCategory } = require("../controllers/category")
 
 router.post("/category", createCategory);
-
+router.post("/subcategory", createSubCategory);
 module.exports = router;
