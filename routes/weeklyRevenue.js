14a6dfb48112c750f44834e4932ba767b8a54cfa
Iqra yasmeen <yasirali4363@gmail.com>
2026-01-29 17:19:20 +0500

    Analytics api test

--- routes/weeklyRevenue.js
+++ routes/weeklyRevenue.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const { weeklyRevenue} = require('../controllers/weeklyRevenue')
+const { weeklyRevenue } = require('../controllers/weeklyRevenue')
 router.get('/weekly-revenue', weeklyRevenue )
 module.exports = router
