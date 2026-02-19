f53fd186ed793c2216eadb68a87b171214ede0d9
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-19 18:17:45 +0500

    just testing

--- routes/adminUser.js
+++ routes/adminUser.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const auth = require("../middleware/authentication");
 const authorize = require("../middleware/authrize");
 
-console.log("testing");
 
 const {
   getAllUsers,
